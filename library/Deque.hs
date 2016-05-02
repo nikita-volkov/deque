766365403a7e5fdf499097ae654793e469160ff6
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-05-02 13:22:19 +0300

    Complexity docs

--- library/Deque.hs
+++ library/Deque.hs
@@ -7,26 +7,36 @@ import qualified BasePrelude
 data Deque a =
   Deque ![a] ![a]
 
+-- |
+-- /O(1)/, sometimes /O(n)/.
 shiftRight :: Deque a -> Deque a
 shiftRight deque =
   fromMaybe deque $
   fmap (\(a, b) -> snoc a b) $
   uncons deque
 
+-- |
+-- /O(1)/, sometimes /O(n)/.
 shiftLeft :: Deque a -> Deque a
 shiftLeft deque =
   fromMaybe deque $
   fmap (\(a, b) -> cons a b) $
   unsnoc deque
 
+-- |
+-- /O(1)/.
 cons :: a -> Deque a -> Deque a
 cons a (Deque snocList consList) =
   Deque (snocList) (a : consList)
 
+-- |
+-- /O(1)/.
 snoc :: a -> Deque a -> Deque a
 snoc a (Deque snocList consList) =
   Deque (a : snocList) (consList)
 
+-- |
+-- /O(1)/, sometimes /O(n)/.
 uncons :: Deque a -> Maybe (a, Deque a)
 uncons (Deque snocList consList) =
   case consList of
@@ -39,6 +49,8 @@ uncons (Deque snocList consList) =
         _ ->
           Nothing
 
+-- |
+-- /O(1)/, sometimes /O(n)/.
 unsnoc :: Deque a -> Maybe (a, Deque a)
 unsnoc (Deque snocList consList) =
   case snocList of
@@ -51,6 +63,8 @@ unsnoc (Deque snocList consList) =
         _ ->
           Nothing
 
+-- |
+-- /O(n)/.
 prepend :: Deque a -> Deque a -> Deque a
 prepend (Deque snocList1 consList1) (Deque snocList2 consList2) =
   Deque snocList3 consList3
@@ -60,14 +74,20 @@ prepend (Deque snocList1 consList1) (Deque snocList2 consList2) =
     consList3 =
       consList1
 
+-- |
+-- /O(1)/.
 reverse :: Deque a -> Deque a
 reverse (Deque snocList consList) =
   Deque consList snocList
 
+-- |
+-- /O(1)/, sometimes /O(n)/.
 tail :: Deque a -> Deque a
 tail =
   fromMaybe <$> id <*> fmap snd . uncons
 
+-- |
+-- /O(1)/, sometimes /O(n)/.
 init :: Deque a -> Deque a
 init =
   fromMaybe <$> id <*> fmap snd . unsnoc
